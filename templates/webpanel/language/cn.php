35a870b1f99187cb336d63602e2d89b03b7df8b2
showdoi <showdoi@126.com>
2021-08-29 16:05:54 +0800

    Create cn.php
    support Chinese Simplified

---
+++ templates/webpanel/language/cn.php
@@ -0,0 +1,15 @@
+<?php
+$lang = array(
+  "passes" => "解码任务列表",
+  "satellite" => "卫星列表",
+  "images" => "解码图像",
+  "pass_start" => "过境开始时间",
+  "pass_end" => "过境结束时间",
+  "max_elev" => "最大仰角",
+  "elev" => "仰角",
+  "page" => "第几",
+  "of" => "总",
+  "prev" => "上一个",
+  "next" => "下一个",
+);
+?>
